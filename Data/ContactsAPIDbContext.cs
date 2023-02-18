2c9e80cc50bd0f5ec31897592b0c8aecac5d92c0
mac <dadebayo200@gmail.com>
2023-02-18 12:41:55 +0100

    Initial commit

---
+++ Data/ContactsAPIDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ContactsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsAPI.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
+
